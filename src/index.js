9fa41bf131fcfea6110b82d1d8751c1d4ac7e28c
eich47 <eich@tut.by>
2021-02-18 16:26:02 +0300

    feat: add implemention of a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    let stack = [];
+
+    const openBrackets = [];
+    const closeBrackets = [];
+
+    bracketsConfig.forEach((item) => {
+        openBrackets.push(item[0]);
+        closeBrackets.push(item[1]);
+    });
+
+    for (const bracket of str) {
+        if (openBrackets.includes(bracket)) {
+            stack.push(bracket);
+        } else if (closeBrackets.includes(bracket)) {
+            const res = stack.pop(bracket);
+
+            if (res === undefined) {
+                return false;
+            }
+
+            const indexClosedBracket = closeBrackets.indexOf(bracket);
+            const openedBrackets = openBrackets[indexClosedBracket];
+            if (res !== openedBrackets) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
